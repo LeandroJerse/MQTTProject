27bbaa33bae7df7fe3f261cd6689f6255e0a6031
Leandro Marques <leandrojerse@gmail.com>
2025-06-03 21:22:16 -0300

    testing moment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Marx!");
+const fs = require('fs')
+
+/*fs.writeFileSync('output.txt', 'E O L, JA FEZ HOJE  ?')
+const arquivo = fs.readFileSync('output.txt', 'utf-8');
+
+console.log(arquivo);*/
+
+const moment = require('moment');
+
+const time = new Date();
+const timeconverted = moment(time).format('DD/MM/YYYY HH:mm:ss');
+console.log(timeconverted);
